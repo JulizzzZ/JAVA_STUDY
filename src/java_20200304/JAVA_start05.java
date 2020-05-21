1c894a0491cd85ac0a6810a4f349fed8a5ada71d
JulizzzZ <JulizzzZ@yeah.net>
2020-05-21 09:54:37 +0800

    集合

---
+++ src/java_20200304/JAVA_start05.java
@@ -0,0 +1,11 @@
+package java_20200304;
+
+public class JAVA_start05 {
+    public static void main(String[] args) {
+        /*继承*/
+        //超类，基类，父类<------>子类，孩子类，派生类
+        //调用超类的方法super
+
+        //不允许拓展的类被称为final类
+    }
+}
